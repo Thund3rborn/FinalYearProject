2d7b9e033ae57b413b441eceb37ecb33b267c311
Thund3rborn <kacper.feister@outlook.com>
2022-12-08 18:51:50 +0000

    Added rectangle placing where the cursor is pointing at

---
+++ Assets/Materials/Scripts/CreateRoad.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CreateRoad : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
+        {
+            GameObject road = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            road.GetComponent<Transform>().localScale = new Vector3(2.0f, 0.1f, 2.0f);
+            road.transform.position = raycastHit.point;
+        }
+
+        //GameObject road = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //road.GetComponent<Transform>().localScale = new Vector3(2.0f, 0.1f, 2.0f);
+        //road.transform.position = new Vector3(0,0,0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
