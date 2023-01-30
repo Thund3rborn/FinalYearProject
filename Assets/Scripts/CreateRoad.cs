0d499f7552ea76576422b6b728b2789f5acd515e
Thund3rborn <kacper.feister@outlook.com>
2023-01-30 19:56:32 +0000

    Added code which is responsible for saving startPos i endPos of the lines being drawn in a list of positions

--- Assets/Scripts/CreateRoad.cs
+++ Assets/Scripts/CreateRoad.cs
@@ -9,19 +9,16 @@ using UnityEngine.UIElements;
 
 public class CreateRoad : MonoBehaviour
 {
-    UnityEngine.Mesh UE_mesh;
-    UnityEngine.Object UE_obj;
-
     public MousePos mousePos;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private Button button;
     private Vector3 startPoint, endPoint;
+    private List<List<Vector3>> listOfPositionLists = new List<List<Vector3>>();
 
     // Start is called before the first frame update
     void Start()
-    { 
-
+    {
     }
 
    // Update is called once per frame
@@ -30,25 +27,35 @@ public class CreateRoad : MonoBehaviour
         CreateRoads();
     }
 
+
+
     void CreateRoads()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
+        List<Vector3> positionsList = new List<Vector3>();
 
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask) && startPoint == Vector3.zero)
         {
             Debug.Log("firstPos");
             startPoint = raycastHit.point;
+            positionsList.Add(startPoint);
         }
         else if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out raycastHit, float.MaxValue, layerMask))
         {
             endPoint = raycastHit.point; Debug.Log("create");
+            positionsList.Add(endPoint);
 
             CreateStraightRoad();
 
             startPoint = Vector3.zero;
+            listOfPositionLists.Add(positionsList);
 
-
+            for (int i = 0; i < listOfPositionLists.Count; i++)
+            {
+                List<Vector3> list = listOfPositionLists[i];
+                Debug.Log("List " + i + ": " + string.Join(", ", list));
+            }
         }
     }
     
