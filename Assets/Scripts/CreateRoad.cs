2d2123876228d6283fc0c346e9ae77ed03c08c61
Thund3rborn <kacper.feister@outlook.com>
2023-01-30 20:26:04 +0000

    Added fix which resolved the problem with the lists that havent been saved continuously

--- Assets/Scripts/CreateRoad.cs
+++ Assets/Scripts/CreateRoad.cs
@@ -10,15 +10,19 @@ using UnityEngine.UIElements;
 public class CreateRoad : MonoBehaviour
 {
     public MousePos mousePos;
+
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private Button button;
+
     private Vector3 startPoint, endPoint;
     private List<List<Vector3>> listOfPositionLists = new List<List<Vector3>>();
+    
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
    // Update is called once per frame
@@ -33,29 +37,32 @@ public class CreateRoad : MonoBehaviour
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        List<Vector3> positionsList = new List<Vector3>();
-
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask) && startPoint == Vector3.zero)
         {
             Debug.Log("firstPos");
             startPoint = raycastHit.point;
-            positionsList.Add(startPoint);
         }
         else if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out raycastHit, float.MaxValue, layerMask))
         {
+            List<Vector3> positionsList = new List<Vector3>();
+
             endPoint = raycastHit.point; Debug.Log("create");
             positionsList.Add(endPoint);
 
+            positionsList.Add(startPoint);
+            listOfPositionLists.Add(positionsList);
+
             CreateStraightRoad();
 
-            startPoint = Vector3.zero;
-            listOfPositionLists.Add(positionsList);
 
             for (int i = 0; i < listOfPositionLists.Count; i++)
             {
                 List<Vector3> list = listOfPositionLists[i];
                 Debug.Log("List " + i + ": " + string.Join(", ", list));
             }
+
+            startPoint = Vector3.zero;
+            endPoint = Vector3.zero;
         }
     }
     
